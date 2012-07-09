74342d96a4dc8f50f753cbd9516587a8b0325098
Robin Abrahamsson <test@test.test>
2012-07-09 19:05:36 +0200

    my first commit

---
+++ color.el
@@ -0,0 +1,19 @@
+;; ========= Set colours ==========
+
+;; Set cursor and mouse-pointer colours
+(set-cursor-color "red")
+(set-mouse-color "goldenrod")
+
+;; Set region background colour
+;;(set-face-background 'region "blue")
+;;(set-face-foreground 'region "green")
+
+;; Set emacs background colour
+(set-background-color "black")
+
+;; Set comment and string color
+'(font-lock-comment-face ((((class color) (min-colors 88) (background dark)) (:foreground "yellow"))))
+'(font-lock-string-face ((((class color) (min-colors 88) (background dark)) (:foreground "darkred"))))
+
+;;Set emacs foregorund color
+(set-foreground-color "green")
---
+++ misc.el
@@ -0,0 +1,8 @@
+;;For setting backspace when usin ssh solaris
+(normal-erase-is-backspace-mode 0)
+
+;;Show column number
+(column-number-mode t)
+
+;;Show parenteses when matching
+(show-paren-mode t)
