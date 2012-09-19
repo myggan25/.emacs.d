808bc635c306e365ccbe4b8ff77d048d067e8500
Robin Abrahamsson <test@test.test>
2012-09-19 17:59:08 +0200

    added some shotcuts

---
+++ shortcuts.el
@@ -0,0 +1,6 @@
+;;===========Global shortcuts
+
+(global-set-key "\C-u" 'control-x-prefix)
+(global-set-key "\M-u" 'execute-extended-command)
+(global-set-key "\C-xo" 'other-window)
+(global-set-key "\C-x C-u" 'undo)
