ef9865458c798c22c6dd319e6e8e089c9faccd1c
Robin Abrahamsson <robin.abrahamsson25@gmail.com>
2014-12-16 18:56:20 +0100

    Added missing stuff from last commit.

---
+++ init/windows_specific.el
@@ -0,0 +1,21 @@
+
+;;=============Allegro CL
+
+; This is sample code for starting and specifying defaults to the
+
+; Emacs-Lisp interface. Should only be commented out if Allegro is installed
+
+; (push "C:/acl82express/eli" load-path)
+
+; (load "fi-site-init.el")
+
+; (setq fi:common-lisp-image-name "C:/acl82express/allegro-express.exe")
+
+; (setq fi:common-lisp-image-file "C:/acl82express/allegro-express.dxl")
+
+; (setq fi:common-lisp-directory "C:/acl82express")
+
+;;;F�r att göra obegränsad utskrift i allegro
+;(setf tpl:*print-length* nil)
+;(fset 'eval-allegro
+;   "\C-x\C-s\C-c\C-b\C-xo\C-c\C-p")
